a96c2b8a580b57dec74f57f4dc538d16b3892c13
septicleon <steckenreuterleon@gmail.com>
2024-12-11 14:04:24 +0100

    3

--- app/src/main/java/com/example/gametracker/MainActivity.java
+++ app/src/main/java/com/example/gametracker/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.gametracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText Username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Username = findViewById(R.id.username_input);
+        password = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+        /* Scenen wechsel// */
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Startseite.class));
+            }
+        });
+
     }
+
 }
